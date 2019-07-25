5d78f01fca851f8318af7fdc7a8ba7e9996128fe
cicerad <andrea.d.cicero@gmail.com>
2019-07-25 18:37:51 -0500

    adding js and css

--- static/js/id_map.js
+++ static/js/id_map.js
@@ -1,48 +1,38 @@
 // Reading and filtering data
 
-
-function createMap() {
-
-  // Define basemaps layers: street, dark, satellite
-    const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-    });
+// Define basemaps layers: street, dark, satellite
+  const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+  });
 
   const darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.dark",
-    accessToken: API_KEY
-    });
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+  });
 
   const satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.satellite",
-    accessToken: API_KEY
-    });
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+  });
 
-    // Define a baseMaps object to hold our base layers
-    const baseMaps = {
-      "Street Map": streetmap,
-      "Dark Map": darkmap,
-      "Satellite Map": satellitemap
-    };
+  // Define a baseMaps object to hold our base layers
+  const baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Satellite Map": satellitemap
+  };
 
-
-    // Create overlay object to hold our overlay layer
-    //const overlayMaps = {
-    //  Earthquakes: earthquakes,
-    //  Plates: plates
-    //};
-
-    // Create our map, giving it the streetmap, darkmap, satellite, earthquakes and plates layers to display on load
+  // Create our map, giving it the streetmap, darkmap, satellite, earthquakes and plates layers to display on load
     const myMap = L.map("map", {
-      center: [44.4604887, -110.8288224],
-      zoom: 3,
+      center: [44.460, -110.828],
+      zoom: 12,
       layers: [streetmap, darkmap, satellitemap]
     });
 
@@ -52,5 +42,3 @@ function createMap() {
     L.control.layers(baseMaps, null, {
       collapsed: false
     }).addTo(myMap);
-
-    createMap();
