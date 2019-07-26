db8782e668470b80f86994fa7fc5378986358b41
cicerad <andrea.d.cicero@gmail.com>
2019-07-25 21:14:40 -0500

    commit Thursday night changes

--- static/js/id_map.js
+++ static/js/id_map.js
@@ -1,5 +1,3 @@
-// Reading and filtering data
-
 // Define basemaps layers: street, dark, satellite
   const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -29,6 +27,7 @@
     "Satellite Map": satellitemap
   };
 
+
   // Create our map, giving it the streetmap, darkmap, satellite, earthquakes and plates layers to display on load
     const myMap = L.map("map", {
       center: [44.460, -110.828],
@@ -39,6 +38,7 @@
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
-    L.control.layers(baseMaps, null, {
+    L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
+}
